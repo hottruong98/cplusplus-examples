7765094222827954a9d652cf6d61a0b1d2b1fc30
Trương Văn Họt <61400834+hottruong98@users.noreply.github.com>
2021-06-30 14:38:10 +0700

    Change data value written to heap memory

--- lesson5_pointers.cpp
+++ lesson5_pointers.cpp
@@ -14,7 +14,7 @@ int main() {
   //What we're asking for is 8 bytes of memory. So this has allocated 8 bytes of memory
   //for us and is returning a pointer to the begining of that block of memory
   char* buffer = new char[8];
-  memset(buffer, 5, 8); //fill a block of memory with data that we specify
+  memset(buffer, 0, 8); //fill a block of memory with data that we specify
 
   *buffer = 6;
   
