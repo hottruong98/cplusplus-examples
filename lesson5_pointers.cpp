6f0632ad2afee5c5c848a586996bf51e68ebf4de
hottruong <truongvanhot98@gmail.com>
2021-06-30 13:46:19 +0700

    Add lesson5_pointers.cpp file

---
+++ lesson5_pointers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#define LOG(x) std::cout << x << std::endl
+
+int main() {
+  /* Working with stack */
+  // int var = 6;
+  // int* ptr = &var;
+  // *ptr = 98;
+  // LOG(var);
+
+  /* Working with heap */
+  //What we're asking for is 8 bytes of memory. So this has allocated 8 bytes of memory
+  //for us and is returning a pointer to the begining of that block of memory
+  char* buffer = new char[8];
+  memset(buffer, 98, 8); //fill a block of memory with data that we specify
+
+  *buffer = 6;
+  
+  char** ptr = &buffer; //pointer to pointer
+  if(*buffer >= 1) {
+
+  }
+  delete[] buffer;
+  std::cin.get();
+  return 0;
+}
