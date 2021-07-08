7d7f5a48b2bb06cd06b534df298ff47222391fae
hottruong <truongvanhot98@gmail.com>
2021-07-08 20:45:56 +0700

    Inserting at HEAD

--- data_structure_algorithm/linked_list_1/taking_input_2.cpp
+++ data_structure_algorithm/linked_list_1/taking_input_2.cpp
@@ -1,4 +1,5 @@
-// Inserting a head
+// Inserting a head (reversing a linked list: keeping tail and moving head)
+// Input: 1 2 3 4 ==> Output: 4->3->->2->1->NULL
 #include <iostream>
 using namespace std;
 
@@ -12,8 +13,33 @@ class Node {
     }
 };
 void printLinkedList(Node* head) {
-    
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data <<"->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+Node* takeInput() {
+    int data;
+    cin >> data;
+    Node* head = NULL;
+    Node* tail = NULL;
+    while(data != -1) {
+        Node* n = new Node(data);
+        if(head == NULL) {
+            head = n;
+            tail = n;
+        } else {
+            n->next = head;
+            head = n;
+        }
+        cin >> data;
+    }
+    return head;
 }
 int main() {
+    Node* head = takeInput();
+    printLinkedList(head);
     return 0;
 }
